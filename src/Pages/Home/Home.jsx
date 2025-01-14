04f3125319d7e9a9b177cb1ccbec7d67acc001c1
Tamim Ahmed <tamim20072@gmail.com>
2025-01-15 03:07:27 +0600

    home page route setup done

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet-async";
+
+const Home = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>AssetEase | Home</title>
+            </Helmet>
+        </div>
+    );
+};
+
+export default Home;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,16 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
-        {
-            path: '/',
-            element: <App/>
-        }
-    ]
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
