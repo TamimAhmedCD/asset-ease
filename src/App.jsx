899b13e9745d755db01b0e0036d3e0aa06e6a944
Tamim Ahmed <tamim20072@gmail.com>
2025-01-14 23:14:56 +0600

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Button } from '@mui/material'
-import './App.css'
+import { Button } from "@mui/material";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-
-      <h1 className='text-3xl'>Vite + React</h1>
-      <Button variant='contained'>Hello World</Button>
+      <h1 className="text-3xl">Vite + React</h1>
+      <Button variant="contained">
+        Hello World
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
