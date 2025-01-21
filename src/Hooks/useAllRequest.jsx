056779f19785442274211092fa64b03c3d8d64f9
Tamim Ahmed <tamim20072@gmail.com>
2025-01-21 23:59:44 +0600

    useAllRequest updat hook

--- src/Hooks/useAllRequest.jsx
+++ src/Hooks/useAllRequest.jsx
@@ -1,16 +1,18 @@
 import useAxiosPublic from "./useAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
+import useAuth from './useAuth';
 
 const useAllRequest = (searchQuery = "") => {
   const axiosPublic = useAxiosPublic();
+  const {user} = useAuth()
 
   const { data: requests = [], refetch, isFetching } = useQuery({
-    queryKey: searchQuery ? ["search-requests", searchQuery] : ["requests"],
+    queryKey: searchQuery ? ["search-requests", searchQuery] : ["requests", user?.email] , 
     queryFn: async () => {
       try {
         if (searchQuery) {
           // Call the search API when searchQuery is provided
-          const res = await axiosPublic.get("/requested-assets", {
+          const res = await axiosPublic.get(`/requested-assets?email=${user.email}`, {
             params: {
               requester_email: searchQuery, 
               requester_name: searchQuery 
@@ -19,7 +21,7 @@ const useAllRequest = (searchQuery = "") => {
           return res.data;
         } else {
           // Call the all requests API when no searchQuery is provided
-          const res = await axiosPublic.get("/requested-assets");
+          const res = await axiosPublic.get(`/requested-assets?email=${user.email}`);
           return res.data;
         }
       } catch (error) {
