01f619bfbd7683805668571625d7ca7aca5ed2dd
Tamim Ahmed <tamim20072@gmail.com>
2025-01-24 22:44:25 +0600

    make use axios secure hook

--- src/Hooks/useEmployeeList.jsx
+++ src/Hooks/useEmployeeList.jsx
@@ -1,16 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
-import useAuth from './useAuth';
-import useAxiosPublic from './useAxiosPublic';
+import useAuth from "./useAuth";
+import useAxiosPublic from "./useAxiosPublic";
 
 const useEmployeeList = () => {
-    const axiosPublic = useAxiosPublic()
-  const {user} = useAuth(); //   useAuth hook
+  const axiosPublic = useAxiosPublic();
+  const { user } = useAuth(); //   useAuth hook
 
   // TanStack query
   const { refetch, data: employeeList = [] } = useQuery({
     queryKey: ["employeeList", user?.email],
     queryFn: async () => {
-      const res = await axiosPublic.get(`/employee-accounts/${user?.email}`);
+      const res = await axiosPublic.get(`/employee-accounts/${user?.email}`, {headers: {
+        authorization: `Bearer ${localStorage.getItem('access-token')}`
+      }});
       return res.data;
     },
   });
