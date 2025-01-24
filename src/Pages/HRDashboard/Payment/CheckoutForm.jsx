230ab5cd18c6722fd169b2b03e1e56091c3793b6
Tamim Ahmed <tamim20072@gmail.com>
2025-01-25 03:05:05 +0600

    CheckoutForm done

---
+++ src/Pages/HRDashboard/Payment/CheckoutForm.jsx
@@ -0,0 +1,45 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { Button } from '@mui/material';
+
+const CheckoutForm = () => {
+    const stripe = useStripe()
+    const elements = useElements()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if(!stripe || !elements) {
+        return 
+    }
+    const card = elements.getElement(CardElement)
+
+    if(card === null) {
+        return
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: '16px',
+              color: '#424770',
+              '::placeholder': {
+                color: '#aab7c4',
+              },
+            },
+            invalid: {
+              color: '#9e2146',
+            },
+          },
+        }}
+      />
+        <div className="flex justify-center items-center mt-6"><Button variant="contained" type="submit" disabled={!stripe} className="text-center">Subscribe</Button></div>
+      </form>
+    </div>
+  );
+};
+
+export default CheckoutForm;
--- src/Pages/HRDashboard/Payment/Payment.jsx
+++ src/Pages/HRDashboard/Payment/Payment.jsx
@@ -1,9 +1,19 @@
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
-    return (
-        <div>
-            <h1 className="text-2xl text-center">This is Payment page</h1>
-        </div>
-    );
+  return (
+    <div className="w-6/12 mx-auto">
+      <h1 className="text-2xl text-center">This is Payment page</h1>
+      <div className="mt-10">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm></CheckoutForm>
+        </Elements>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
